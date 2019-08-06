7438c1afcba3bf2f608dd7a2685f61834a4b0378
Stella <stellaywu@gmail.com>
2019-08-06 01:31:25 -0400

    completed task

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import './App.css';
+import Todos from './components/Todos';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'eat breakfast',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'eat lunch',
+        completed: false
+      },
+       {
+         id: 3,
+         title: 'eat dinner',
+         completed: false
+       },
+    ]
+  }
+
+//Toggle Complete
+  markComplete = (id) =>{
+    this.setState({ todos: this.state.todos.map(todo =>{
+      if(todo.id ===id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+      })
+    }
+    );
+  }
+  render(){
+    console.log(this.state.todos)
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+      </div>
+    );
+  }
+}
+
+export default App;
